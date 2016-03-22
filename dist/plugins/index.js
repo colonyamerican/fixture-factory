6522d261a0a5cad35643438fbf2f87e80cc6df6c
bosgood <bosgood@gmail.com>
2016-03-22 11:08:30 -0400

    Rebuild project dist

---
+++ dist/plugins/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Reference = undefined;
+
+var _reference = require('./reference');
+
+var _reference2 = _interopRequireDefault(_reference);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Reference = exports.Reference = _reference2.default;
+
+exports.default = {
+  Reference: Reference
+};
